1a750125329a4cf92c4b827770988306faf4ace8
Muano <muano.m365@gmail.com>
2024-03-13 10:05:09 +0200

    Added content and basic spacing

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -1,6 +1,45 @@
+import './Portfolio.css'
+
 const Portfolio = () => {
    return (
-      <h1>Portfolio</h1>
+      
+      <div id='portfolio' >
+         <h1 id='page-title' >
+            Welcome to my portfolio
+         </h1>
+         <section id='education' >
+            <h3>
+               Educational Background
+            </h3>
+            <h1>
+               University of the Witwatersrand
+            </h1>
+            <div id='uni-majors' >
+               <div id='math' >
+                  <h3>
+                     Math
+                  </h3>
+                  <p>
+                     Algebra I, Calculus I, Multivariable Calculus, Basic Analysis, Transition to Abstract Mathematics II, Abstract Mathematics II, Linear Algebra II, Introduction to Mathematical Statistics, Positive Linear Systems III (Leontif Systems), Group Theory III, Real Analysis III, Complex Analysis III
+                  </p>
+               </div>
+               <div id='comp' >
+                  <h3>
+                     Computer Science
+                  </h3>
+                  <p>
+                     Computational and Applied Mathematics I, Basic Computer Organisation I, Discrete Computational Structures I, Introduction to Data Structures and Algorithms I, Introduction to Algorithms and Programming I, Database Fundamentals II, Mobile Computing II, Analysis of Algorithms II, Computer Networks II, Formal Languages and Automata, Advanced Analysis of Algorithms III, Software Design III, Operating Systems and System Programming III
+                  </p>
+               </div>
+               <div id='econ-and-fin' >
+                  <h3>Economics and Finance</h3>
+                  <p>
+                     Economic Theory 1A - Microeconomics for Economists, Economic Theory 1B - Macroeconomics for Economists, Corporate Finance, Investment II
+                  </p>
+               </div>
+            </div>
+         </section>
+      </div>
    );
 }
  
