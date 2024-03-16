fd8a8ccbb98a40a7753c9110f32744025258284e
Muano <muano.m365@gmail.com>
2024-03-16 16:38:17 +0200

    Refactored the Home page animations, css classes and some of the content

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,13 @@ import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useGSAP } from '@gsap/react';
 import './Home.css'
+import { useRef } from 'react';
 
 gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 const Home = () => {
+
+   const tl = useRef()
 
    let myBlogs = {
       '1': {
@@ -29,8 +32,10 @@ const Home = () => {
       const mm = gsap.matchMedia()
 
       mm.add('(max-width: 479px)', () => {
+
+         // Welcome
          gsap.timeline()
-         .from('.welcome-message', {
+         .from('#welcome-message', {
             scale: 0,
             duration: 1.8,
             ease: 'bounce'
@@ -41,90 +46,57 @@ const Home = () => {
             text: "",
          }, '>-0.9')
 
-         gsap.set([ '#p2', '#p3' ], {
-            opacity: 0,
-            top: '150px'
+         // Introduction
+         const introductionParagraphs = document.querySelectorAll('.introduction-p')
+         introductionParagraphs.forEach(p => {
+            if (!p.id.includes('p1')) {
+               gsap.set('#' + p.id, {
+                  opacity: 0,
+                  top: '300px'
+               })
+            }
          })
-         gsap.to('#p2', {
-            scrollTrigger: {
-               trigger: '#p2',
-               start: 'top 100%',
-               end: 'top 40%',
-               toggleActions: "play none none reverse",
-               scrub: true
-            },
-            top: '0',
-            opacity: 1,
-            ease: 'sine.inOut',
-         })
-
-         gsap.to('#p3', {
-            scrollTrigger: {
-               trigger: '#p3',
-               start: 'top 100%',
-               end: 'top 40%',
-               toggleActions: "play none none reverse",
-               scrub: true
-            },
-            top: '0',
-            opacity: 1,
-            ease: 'sine.inOut',
+         introductionParagraphs.forEach(p => {
+            if (!p.id.includes('p1')) {
+               gsap.to('#' + p.id, {
+                  scrollTrigger: {
+                     trigger: '#' + p.id,
+                     start: 'top 100%',
+                     toggleActions: "play none none reverse",
+                  },
+                  top: '0',
+                  opacity: 1,
+                  ease: 'sine.inOut',
+                  duration: 1.2
+               })
+            }
          })
 
-         gsap.from([ '.latest-blogs h2', '.latest-blogs div' ], {
+         // Latest Blogs
+         gsap.from('#latest-blogs div', {
             scrollTrigger: {
-               trigger: '.latest-blogs h2',
+               trigger: '#latest-blogs h2',
                start: 'top 70%',
                toggleActions: "play none none reverse",
             },
             rotateY: '-90deg',
-            duration: 1,
-            ease: 'sine.inOut',
+            duration: 1.5,
+            ease: 'power1.inOut',
             stagger: 0.15
          })
 
-         gsap.from('.services h2', {
-            scrollTrigger: {
-               trigger: '.services h2',
-               start: 'top 85%',
-               toggleActions: "play none none reverse",
-            },
-            rotateY: '-90deg',
-            duration: 0.5,
-            ease: 'sine.inOut',
-         })
-
-         gsap.from('#services-1', {
-            scrollTrigger: {
-               trigger: '#services-1',
-               start: 'top 95%',
-               toggleActions: "play none none reverse",
-            },
-            rotateX: '90deg',
-            duration: 0.5,
-            ease: 'sine.inOut',
-         })
-
-         gsap.from('#services-2', {
-            scrollTrigger: {
-               trigger: '#services-2',
-               start: 'top 95%',
-               toggleActions: "play none none reverse",
-            },
-            rotateX: '90deg',
-            duration: 0.5,
-            ease: 'sine.inOut',
-         })
-
-         gsap.from('#services-3', {
-            scrollTrigger: {
-               trigger: '#services-3',
-               start: 'top 95%',
-               toggleActions: "play none none reverse",
-            },
-            rotateX: '90deg',
-            duration: 0.5,
-            ease: 'sine.inOut',
+         // Services
+         document.querySelectorAll('.service').forEach(service => {
+            gsap.from('#' + service.id, {
+               scrollTrigger: {
+                  trigger: '#' + service.id,
+                  start: 'top 95%',
+                  toggleActions: "play none none reverse",
+               },
+               rotateX: '90deg',
+               duration: 0.5,
+               ease: 'sine.inOut',
+            })
          })
       })
    })
@@ -133,39 +105,43 @@ const Home = () => {
 
    return (
       <div id='home-container'>
-         {/* <section id='home-bg'>
-            <div id='circle-1'></div>
-            <div id='circle-2'></div>
-            <div id='square-1'></div>
-            <div id='square-2'></div>
-         </section> */}
-         <section
-            className='welcome-section'
-         >
-            <div className='welcome-message'>
-               <div id='wm-part-1' >
-                  <h1>Hi, welcome to my digital space. My name is</h1>
-                  <h1>&lt; <span id='name'>Muano</span> /&gt;</h1>
-                  <h1>and I'm thrilled to have you here!</h1>
-               </div>                           
+
+         <section id='welcome' >
+
+            <div id='welcome-message'>
+
+               <h1>Hi, welcome to my digital space. My name is</h1>
+            
+               <h1>&lt; <span id='name'>Muano</span> /&gt;</h1>
+
+               <h1>and I'm thrilled to have you here!</h1>
+
             </div>
+
          </section>
 
-         <section className='intro'>
-            <h3>A little bit about myself</h3>
-            <p id='p1'>
+         <section id='introduction'>
+
+            <h3 className='section-title' >A little bit about myself</h3>
+
+            <p id='introduction-p1' className='introduction-p'>
                I am a very passionate and versatile techie. I wear multiple hats in the tech industry—being a full-stack developer, a data analyst, and an up coming penetration tester.
             </p>
-            <p id='p2'>
+
+            <p id='introduction-p2' className='introduction-p'>
                This website is your gateway to exploring my journey and the diverse range of skills I bring to the table. Whether you're interested in innovative software projects, cutting-edge data science solutions, or the intricacies of cybersecurity, you'll find it all here.   
             </p>
-            <p id='p3'>
+
+            <p id='introduction-p3' className='introduction-p'>
                Take a stroll through my portfolio, delve into my blogs for insightful articles, and feel free to reach out if you share a passion for technology or if you're looking to collaborate on exciting projects. Let's connect, explore, and innovate together!
             </p>
+
          </section>
 
-         <section className='latest-blogs' >
-            <h2>Latest blogs</h2>
+         <section id='latest-blogs' >
+
+            <h2 className='section-title' >Latest blogs</h2>
+
             {
                Object.values(myBlogs).map(blog => (
                   <div key={ blog.id } >
@@ -174,11 +150,14 @@ const Home = () => {
                   </div>
                ))
             }
+
          </section>
 
-         <section className='services' >
-            <h2>Services</h2>
-            <div id='services-1' >
+         <section id='services' >
+
+            <h2 className='section-title' >Services</h2>
+
+            <div id='service-1' className='service' >
                <h1>Software Engineering</h1>
                <ul>
                   <li>Software Development</li>
@@ -189,24 +168,15 @@ const Home = () => {
                </ul>
             </div>
 
-            <div id='services-2' >
+            <div id='service-2' className='service' >
                <h1>Data Analytics</h1>
                <ul>
                   <li>Statistical Analysis</li>
                   <li>Data Visualisation</li>
                   <li>Building Models</li>
-                  {/* <li>Data Pipelines</li> */}
                </ul>
             </div>
 
-            <div id='services-3' >
-               <h1>Cybersecurity</h1>
-               <ul>
-                  <li>Pen Testing / Ethical Hacking</li>
-                  <li>Computer Networking</li>
-                  {/* <li>IDS/IPS</li> */}
-               </ul>
-            </div>
          </section>
       </div>
    );
