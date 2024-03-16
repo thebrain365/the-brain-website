9cb3cbd6bb6f2311396c55c278b72e449553ace2
Muano <muano.m365@gmail.com>
2024-03-16 10:19:18 +0200

    Installed gulp, gulp-sass, and sass. Configured the gulp tasks. Added scss files for pages, components, index file and other partials

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series } = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+
+function buildCSS() {
+   return src('./src/assets/styles/index.scss')
+      .pipe(sass())
+      .pipe(dest('./src'))
+}
+
+function watchTask() {
+   watch(['./src/assets/styles/index.scss'], buildCSS)
+}
+
+exports.default = series(buildCSS, watchTask)
