6cb2d546d530cf5f0489ed49aebd4f7888508885
Muano <muano.m365@gmail.com>
2024-03-16 16:55:46 +0200

    Refactored the Services page css classes and some of the content

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -4,20 +4,25 @@ const Services = () => {
    return (
       <div id='services' >
          <section id='software-engineering' >
-            <h1>Software Engineering</h1>
-            <p>
-               As a recent graduate in software engineering with a specialization in full-stack development, I am excited to embark on my journey in the world of technology. Equipped with a solid foundation in both front-end and back-end development, I am eager to contribute my skills to creating impactful digital solutions.
-            </p>
-            <p>
-               I have a knack for developing robust REST APIs that power dynamic web applications and mobile apps. Through coursework and hands-on projects, I've gained experience in building scalable and efficient backend systems that meet the demands of modern software development.
-            </p>
-            <p>
-               My passion for design and user experience fuels my ability to craft intuitive and visually appealing interfaces that resonate with users. From wireframing to prototyping, I enjoy the creative process of bringing ideas to life and ensuring that every pixel serves a purpose.
-            </p>
-            <p>
-               As I embark on my professional journey, I am eager to continue learning and growing in this ever-evolving field. With a curious mindset and a passion for innovation, I am confident in my ability to make a meaningful impact as a software engineer, creating transformative digital experiences that enrich the lives of others.
-            </p>
-            <div>
+
+            <h1 className='service-title' >Software Engineering</h1>
+
+            <div className='service-description' >
+               <p>
+                  As a recent graduate in software engineering with a specialization in full-stack development, I am excited to embark on my journey in the world of technology. Equipped with a solid foundation in both front-end and back-end development, I am eager to contribute my skills to creating impactful digital solutions.
+               </p>
+               <p>
+                  I have a knack for developing robust REST APIs that power dynamic web applications and mobile apps. Through coursework and hands-on projects, I've gained experience in building scalable and efficient backend systems that meet the demands of modern software development.
+               </p>
+               <p>
+                  My passion for design and user experience fuels my ability to craft intuitive and visually appealing interfaces that resonate with users. From wireframing to prototyping, I enjoy the creative process of bringing ideas to life and ensuring that every pixel serves a purpose.
+               </p>
+               <p>
+                  As I embark on my professional journey, I am eager to continue learning and growing in this ever-evolving field. With a curious mindset and a passion for innovation, I am confident in my ability to make a meaningful impact as a software engineer, creating transformative digital experiences that enrich the lives of others.
+               </p>
+            </div>
+
+            <div className='service-tools' >
                <h3>
                   Tech Stack
                </h3>
@@ -53,30 +58,48 @@ const Services = () => {
          </section>
 
          <section id='database-design' >
-            <h1>Database Design and Implementation</h1>
-            <p>
-               I can also design and implement relational databases using ER modeling, advanced data modeling, and SQL. Throughout my coursework and hands-on projects, I've developed an understanding of database architecture, ensuring efficient and scalable solutions. By applying normalization techniques, I can optimize database structures for performance and maintainability, eliminating redundancies and update anomalies.
-            </p>
-            <p>
-               Advance in SQL, I have the ability to write complex queries, manipulate data, and optimize database performance. This skill allows me to extract valuable insights from data and drive informed decision-making. With a passion for innovation and a commitment to continuous learning, I want to contribute effectively to real-world projects, leveraging my expertise to create robust and reliable database solutions that empower organizations to unlock the full potential of their data.
-            </p>
+
+            <h1 className='service-title' >Database Design and Implementation</h1>
+
+            <div className='service-description' >
+               <p>
+                  I can also design and implement relational databases using ER modeling, advanced data modeling, and SQL. Throughout my coursework and hands-on projects, I've developed an understanding of database architecture, ensuring efficient and scalable solutions. By applying normalization techniques, I can optimize database structures for performance and maintainability, eliminating redundancies and update anomalies.
+               </p>
+               <p>
+                  Advance in SQL, I have the ability to write complex queries, manipulate data, and optimize database performance. This skill allows me to extract valuable insights from data and drive informed decision-making. With a passion for innovation and a commitment to continuous learning, I want to contribute effectively to real-world projects, leveraging my expertise to create robust and reliable database solutions that empower organizations to unlock the full potential of their data.
+               </p>
+            </div>
+
+            <div className='service-tools' >
+               <h3>
+                  Tech Stack
+               </h3>
+            </div>
          </section>
          <section id='data-analytics' >
-            <h1>Data Analytics</h1>
-            <p>
-               I possess a strong foundation in data analysis, spanning data framing, exploration, interpretation, modeling, and visualization. Through coursework and practical exposure, I've honed my ability to structure datasets effectively, explore patterns and relationships, and derive actionable insights.
-            </p>
-            <p>
-               My experience includes employing statistical methods and visualization tools to uncover insights, interpret findings, and communicate results effectively. Additionally, I've delved into various modeling techniques, including regression analysis and machine learning to make informed predictions and drive decision-making processes.
-            </p>
-            <p>
-               To achieve all this I use tools like:
-            </p>
-            <ul>
-               <li>Python</li>
-               <li>Microsoft Excel</li>
-               <li>Microsoft Powerpoint</li>
-            </ul>
+
+            <h1 className='service-title' >Data Analytics</h1>
+
+            <div className='service-description' >
+               <p>
+                  I possess a strong foundation in data analysis, spanning data framing, exploration, interpretation, modeling, and visualization. Through coursework and practical exposure, I've honed my ability to structure datasets effectively, explore patterns and relationships, and derive actionable insights.
+               </p>
+               <p>
+                  My experience includes employing statistical methods and visualization tools to uncover insights, interpret findings, and communicate results effectively. Additionally, I've delved into various modeling techniques, including regression analysis and machine learning to make informed predictions and drive decision-making processes.
+               </p>
+            </div>
+
+            <div className='service-tools' >
+               <h3>
+                  Tech Stack
+               </h3>
+               <ul>
+                  <li>Python</li>
+                  <li>Microsoft Excel</li>
+                  <li>Microsoft Powerpoint</li>
+               </ul>
+            </div>
+
          </section>
       </div>
    );
