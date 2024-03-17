0a06079eb0eb64adbc902578ffa446057d98aa90
Muano <muano.m365@gmail.com>
2024-03-17 11:48:38 +0200

    Removed the css stylins inside components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
+// import './App.css'
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
