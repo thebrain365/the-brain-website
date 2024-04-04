c5b223047b549d918ba47fe48d50600801e37ffb
Muano <muano.m365@gmail.com>
2024-04-04 20:54:04 +0200

    Added styling for code blocks

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,12 +1,22 @@
 import { BlocksRenderer } from '@strapi/blocks-react-renderer';
 import { useParams } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+import html from 'highlight.js/lib/languages/xml';
+import 'highlight.js/styles/night-owl.css'
 
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('html', html);
 
 const Blog = (props) => {
    let { blogId } = useParams()
    let { data: blog } = useFetch('http://192.168.18.250:1337/api/blogs/' + blogId)
-   // console.log(blog)
+
+   hljs.configure({
+      ignoreUnescapedHTML: true,
+   })
+   hljs.highlightAll()
    return (
       <div id='blog-view'>
          {
